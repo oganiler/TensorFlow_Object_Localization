c9dcfdf4a745d7873b60a3e9798e9bb2918534a1
onur.ganiler <onur.ganiler@gmail.com>
2026-03-05 23:44:58 +0000

    Normalize line endings to LF

--- __main__.py
+++ __main__.py
@@ -1,5 +1,8 @@
 # __main__.py
 # your existing code below
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2' #ignores some warnings
+
 from object_localizer.cli import main
 
 if __name__ == "__main__":
